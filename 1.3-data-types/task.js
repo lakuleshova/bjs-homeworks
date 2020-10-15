c20b3c52dd74f974665fde9428f201c5c36dcc73
Lyudmila Kuleshova <lakuleshova92@gmail.com>
2020-10-15 17:09:01 +0300

    Домашнее задание 3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    let credit = amount - contribution;
+    let months = date.getMonth() - new Date().getMonth() + ((date.getFullYear() - new Date().getFullYear()) * 12);
+    let monthlyPercent = percent / 12 / 100;
+    let monthlyPayment = credit * (monthlyPercent + monthlyPercent / (Math.pow((1 + monthlyPercent), months) - 1));
+    let totalAmount = monthlyPayment * months;
+
+    return parseFloat(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+
+    if (name == "" || name == "null" || name == "undefined") {
+        greeting = `Привет, мир! Меня зовут Аноним`
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}`
+    }
+    return greeting;
 }
