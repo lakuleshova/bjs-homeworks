20a21d20955dfb7015c46dd7c9401c62b36b0d86
Lyudmila Kuleshova <lakuleshova92@gmail.com>
2020-10-20 17:11:00 +0300

    Исправление ДЗ-3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,23 +2,31 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    let credit = amount - contribution;
-    let months = date.getMonth() - new Date().getMonth() + ((date.getFullYear() - new Date().getFullYear()) * 12);
-    let monthlyPercent = percent / 12 / 100;
-    let monthlyPayment = credit * (monthlyPercent + monthlyPercent / (Math.pow((1 + monthlyPercent), months) - 1));
-    let totalAmount = monthlyPayment * months;
+    let totalAmount;
+    if (isNaN(percent)) {
+        totalAmount = `Параметр Процентная ставка содержит неправильное значение ${percent}`;
+    } else if (isNaN(contribution)) {
+        totalAmount = `Параметр Начальный взнос содержит неправильное значение ${contribution}`;
+    } else if (isNaN(amount)) {
+        totalAmount = `Параметр Общая стоимость содержит неправильное значение ${amount}`;
+    } else {
+        let credit = amount - contribution;
+        const currentDate = new Date();
+        let months = date.getMonth() - currentDate.getMonth() + ((date.getFullYear() - currentDate.getFullYear()) * 12);
+        let monthlyPercent = percent / 12 / 100;
+        let monthlyPayment = credit * (monthlyPercent + monthlyPercent / (Math.pow((1 + monthlyPercent), months) - 1));
+        let total = monthlyPayment * months;
+        totalAmount = parseFloat(total.toFixed(2));
+    }
 
-    return parseFloat(totalAmount.toFixed(2));
+    return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     let greeting;
-
-    if (name == "" || name == "null" || name == "undefined") {
-        greeting = `Привет, мир! Меня зовут Аноним`
-    } else {
-        greeting = `Привет, мир! Меня зовут ${name}`
-    }
+    let result = (name == '' || name == null || name == undefined) ? 'Аноним' : name;
+    greeting = `Привет, мир! Меня зовут ${result}`;
+    
     return greeting;
 }
